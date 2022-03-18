0cf2a593fbc24399325e0598b30880bbe89c71eb
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2022-03-17 23:17:52 -0700

    add articles listing feature

--- alpha_blog_1/app/controllers/articles_controller.rb
+++ alpha_blog_1/app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id]) # params[:id] is the id in the url which is a params. This is how we retrieve the article
+    end
 
+    def index
+        @articles = Article.all
     end
 end
--- alpha_blog_1/config/routes.rb
+++ alpha_blog_1/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show] # gives us all of the routes we want for articles - in terminal rails routes --expanded
+  resources :articles, only: [:show, :index] # gives us all of the routes we want for articles - in terminal rails routes --expanded
+
+  def index
+    @articles = Article.all
+  end
 end
