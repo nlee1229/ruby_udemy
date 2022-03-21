7612de52da7e3ed95834c51e20ad0c7cbc19fbfc
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2022-03-20 19:00:53 -0700

    added more crud commands

--- alpha_blog_1/app/controllers/articles_controller.rb
+++ alpha_blog_1/app/controllers/articles_controller.rb
@@ -6,4 +6,32 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new 
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id]) 
+    end
+
+    def update
+        @article = Article.find(params[:id]) # creating an instance of an article and findig it by id
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def create                                                  # keys that we want to permit to be used to create article instance
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was created successfully!"
+        redirect_to @article # rails extracts id from @article 
+        else
+        render 'new' # renders new article template if article does not save
+        end
+    end
 end
