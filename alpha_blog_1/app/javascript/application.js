a6d675ddba3545fd6be01faccd16684e26a960c9
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2022-03-29 23:13:23 -0700

    added navbar and bootstrap

--- alpha_blog_1/app/javascript/application.js
+++ alpha_blog_1/app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "bootstrap"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()s
